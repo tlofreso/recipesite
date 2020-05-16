8421d9cd0f7b865b75806f62b0e96deafba30720
Tony Lofreso Jr <tonylofresojr@gmail.com>
2020-05-15 22:07:31 -0400

    change some of the  formatting, and added a supported sites list.

--- recipe/recipe/settings.py
+++ recipe/recipe/settings.py
@@ -15,11 +15,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ADMIN_ENABLED = False
 
-ALLOWED_HOSTS = ["192.241.152.20"]
+ALLOWED_HOSTS = ["192.241.152.20", "localhost", "192.168.128.98"]
 
 
 # Application definition
